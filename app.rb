c1cd1e57c0c43075b91213f8d8c212008dcfdf9c
dtan4 <dtan4@mac>
2013-06-03 01:26:21 +0900

    Enable sinatra-reloader

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@
 require 'sinatra/base'
 
 class KakidameApp < Sinatra::Base
+  configure :development do
+    Bundler.require :development
+    register Sinatra::Reloader
+  end
+
   get '/' do
     "Kakidame"
   end
