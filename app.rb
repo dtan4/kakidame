51ac42266c7f14099fb5404e91442b7573ba6d9c
dtan4 <dtan4@arch>
2013-06-03 13:18:26 +0900

    Read KAKIDAME_ROOT from ~/.kakidame

--- app.rb
+++ app.rb
@@ -3,13 +3,20 @@
 
 require 'sinatra/base'
 
+CONFIG_PATH = ENV['HOME'] + '/.kakidame'
+
 class KakidameApp < Sinatra::Base
+  configure do
+    load CONFIG_PATH if File.exists?(CONFIG_PATH)
+    KAKIDAME_ROOT = ENV['HOME'] + '/memo' unless defined? KAKIDAME_ROOT
+  end
+
   configure :development do
     Bundler.require :development
     register Sinatra::Reloader
   end
 
   get '/' do
-    "Kakidame"
+    KAKIDAME_ROOT
   end
 end
